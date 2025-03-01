d3738003dc15b51546e90bc0f0511ffceb425df1
kushal gowda P <kushalgowda8586@gmail.com>
2025-03-01 10:41:09 +0530

    done

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
-import AboutUs from '../src/page/aboutus'
-import React from 'react'
+import './App.css';
+import AboutUs from '../src/page/aboutus';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Entity from './components/dummy2';
 
 export const App = () => {
   return (
-    <>
-    <AboutUs/>
-    </>
-  )
-}
-export default App
+    <Router>
+      <>
+      <Routes>
+        <Route path='/' element={<AboutUs />}/>
+        <Route path="/entity" element={<Entity />} />
+        </Routes>
+      </>
+    </Router>
+  );
+};
+
+export default App;
